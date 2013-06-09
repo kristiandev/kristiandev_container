08bc2fe6115a28ec6694263ad9806659bc063332
kristiandev <kristian123cloma@gmail.com>
2013-06-09 03:45:00 -0700

    commit index.php

---
+++ tictactoe/index.php
@@ -0,0 +1,26 @@
+<?php  
+
+?>
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+		<script type="text/javascript" src="jquery-latest.js"></script>
+		<script type="text/javascript" src="script.js"></script>
+	</head>
+	<body>
+		<div id="board">
+			<div class="box box-1" box="1"></div>
+			<div class="box box-2" box="2"></div>
+			<div class="box box-3" box="3"></div>
+			
+			<div class="box box-4" box="4"></div>
+			<div class="box box-5" box="5"></div>
+			<div class="box box-6" box="6"></div>
+			
+			<div class="box box-7" box="7"></div>
+			<div class="box box-8" box="8"></div>
+			<div class="box box-9" box="9"></div>
+		</div>
+		<!--<div id="turn-status">0</div>-->
+	</body>
+</html> 
